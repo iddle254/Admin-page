f72a8ac173f732aee67f469e48cbc8bfb97105a2
Moss <charlesiddle99@gmail.com>
2021-06-26 18:37:51 +0300

    finished the admin master page setup

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/admin', function () {
+    return view('admin.index');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('admin/users', 'App\Http\Controllers\AdminUsersController');
